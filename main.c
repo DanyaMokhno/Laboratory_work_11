d830307c0db0b421bc06d2c1dd660312ea8e842f
DanyaMokhno <d.mohno@rambler.ru>
2024-03-31 19:04:37 +0300

    var 1

---
+++ main.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef union IPv4
+{
+    unsigned short address[4];
+    struct
+    {
+        int : 6;
+        int class: 2;
+    };
+} IPv4;
+
+void get_ip(IPv4 *ip)
+{
+    printf("input ip address\n");
+    scanf("%hd.%hd.%hd.%hd", &(ip->address[0]), &(ip->address[1]), &(ip->address[2]), &(ip->address[3]));
+}
+
+void short_to_bin(unsigned short b)
+{
+    int i;
+    for (i = 7; i >= 0; i--) printf("%d", (b >> i) & (0x1));
+}
+
+void change_class_of_ip(IPv4 *ip)
+{
+//    printf("-----------------------------\n");
+//    short_to_bin(ip->class);
+//    putchar('\n');
+    ip->class = ip->class ^ (1 << 0x1);
+    if ((ip->class & 0b10)) ip->class = ip->class & 0b10;
+//    printf("%d\n", ip->class);
+//    short_to_bin(ip->class);
+//    putchar('\n');
+//    printf("-----------------------------\n");
+}
+
+void show_ip(IPv4 *ip)
+{
+    int i;
+    printf("%hd.%hd.%hd.%hd\n", ip->address[0], ip->address[1], ip->address[2], ip->address[3]);
+    for (i = 0; i < 4; i++)
+    {
+        short_to_bin(ip->address[i]);
+        putchar('.');
+    }
+    putchar(8);
+    putchar('\n');
+}
+
+int main()
+{
+    IPv4 *ip;
+
+    ip = malloc(sizeof(IPv4));
+    get_ip(ip);
+    show_ip(ip);
+    change_class_of_ip(ip);
+    show_ip(ip);
+    free(ip);
+
+    return 0;
+}
+/*
+Class A:
+
+10.0.0.1
+10.20.30.40
+10.100.100.100
+123.45.67.89
+
+Class B:
+
+172.16.0.1
+172.17.20.50
+172.31.255.254
+*/
